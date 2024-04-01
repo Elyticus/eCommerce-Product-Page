35010817f8955ad7202d1eaf66a6af99e3c83434
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-04-01 15:25:34 +0300

    updates

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const menuList = document.getElementById("menuList");
 const navBar = document.getElementById("navBar");
 const cartButton = document.getElementById("cartNavbar");
 const cartMenu = document.getElementById("cartAddButtonNavbar");
+const closePictureBtn = document.getElementById("closeProductBtn");
+const productSection = document.getElementById("productSection");
+const pictures = document.querySelectorAll(".picture_catalog");
+const sliderType = document.getElementById("sliderType");
+const arrowNext = document.getElementById("arrowNext");
+const arrowPrev = document.getElementById("arrowPrev");
 
 menuNavbar.addEventListener("click", () => {
   document.body.classList.toggle("isBlur");
@@ -17,4 +23,22 @@ closeButton.addEventListener("click", () => {
 
 cartButton.addEventListener("click", () => {
   cartMenu.classList.toggle("cart-container");
+});
+
+function showImage(index) {
+  pictures.forEach((picture, i) => {
+    if (i === index) {
+      picture.style.display = "block";
+      sliderType.style.display = "block";
+      productSection.style.display = "block";
+      arrowNext.style.display = "block";
+      arrowPrev.style.display = "block";
+    } else {
+      picture.style.display = "none";
+    }
+  });
+}
+
+closePictureBtn.addEventListener("click", () => {
+  productSection.style.display = "none";
 });
