4a9289d05eaf48b7ea642431ab7d37386c307b3a
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-04-04 00:05:11 +0300

    cart

--- script.js
+++ script.js
@@ -47,32 +47,64 @@ closePictureBtn.addEventListener("click", () => {
 
 // Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to the arrow buttons
-
-  // Get references to the images
-  const images = document.querySelectorAll(".picture_catalog");
-
   let currentIndex = 0; // Initialize the current index
 
   // Add click event listener to the previous button
   prevButton.addEventListener("click", function () {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // Calculate the new index
+    currentIndex = (currentIndex - 1 + pictures.length) % pictures.length; // Calculate the new index
     showImage(currentIndex); // Show the image at the new index
   });
 
   // Add click event listener to the next button
   nextButton.addEventListener("click", function () {
-    currentIndex = (currentIndex + 1) % images.length; // Calculate the new index
+    currentIndex = (currentIndex + 1) % pictures.length; // Calculate the new index
     showImage(currentIndex); // Show the image at the new index
   });
 
   // Function to show the image at a specific index
   function showImage(index) {
     // Hide all images
-    images.forEach(function (img) {
+    pictures.forEach(function (img) {
       img.style.display = "none";
     });
     // Show the image at the given index
-    images[index].style.display = "block";
+    pictures[index].style.display = "block";
+  }
+});
+
+// Add to cart function____________________________________________________
+
+const productQuantity = document.getElementById("productQuantity");
+const plusButton = document.getElementById("plusBtn");
+const minusButton = document.getElementById("minusBtn");
+const addCartButton = document.getElementById("addCartButton");
+const cartMessage = document.getElementById("cartMessage");
+const cartNav = document.getElementById("displayCartProduct");
+const displayCartItems = document.querySelector(".display_items");
+
+let quantity = 0;
+
+productQuantity.innerText = "0";
+
+function updateQuantity(value) {
+  quantity += value;
+  productQuantity.innerText = quantity;
+}
+
+plusButton.addEventListener("click", () => {
+  updateQuantity(1);
+});
+
+minusButton.addEventListener("click", () => {
+  if (quantity > 0) {
+    updateQuantity(-1);
+  }
+});
+
+addCartButton.addEventListener("click", () => {
+  if (quantity > 0) {
+    cartMessage.innerText = quantity.toString();
+    cartNav.innerText = quantity.toString();
+    displayCartItems.style.display = "block";
   }
 });
